ba95329c659739f74c20d99b915dd67e78cc18d3
Gary Pang <gary.pang@gmail.com>
2015-11-29 00:09:19 -0500

    Create "profile" state
    New state allows app to access user's profile and auth data.

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,8 +1,17 @@
 angular.module('angularfireSlackApp')
   .factory('Users', function ($firebaseArray, $firebaseObject, FirebaseUrl) {
       var usersRef = new Firebase(FirebaseUrl+'users');
+      var users = $firebaseArray(usersRef);
 
-      var Users = {};
+      var Users = {
+          getProfile: function (uid) {
+              return $firebaseObject(usersRef.child(uid));
+          },
+          getDisplayName: function (uid) {
+              return users.$getRecord(uid).displayName;
+          },
+          all: users
+      };
 
       return Users;
   });
