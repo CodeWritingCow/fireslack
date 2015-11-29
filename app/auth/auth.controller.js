b7e75a9e32a7465723a3db4f1a8709d0eee87b84
Gary Pang <gary.pang@gmail.com>
2015-11-29 03:59:52 -0500

    Fix bug
    Move functions for user login and registration back inside controller.
    Users can now log in or register properly.

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -7,22 +7,23 @@ angular.module('angularfireSlackApp')
           email: '',
           password: ''
       };
+ 
+      // Authenticates user trying to log in
+      authCtrl.login = function () {
+          Auth.$authWithPassword(authCtrl.user).then(function (auth) {
+              $state.go('home');
+          }, function (error) {
+              authCtrl.error = error;
+          });
+      };
+
+      // Registers new user
+      authCtrl.register = function () {
+          Auth.$createUser(authCtrl.user).then(function (user) {
+              authCtrl.login();
+          }, function (error) {
+              authCtrl.error = error;
+          });
+      };
+ 
   });
-
-  // Authenticates user trying to log in
-  authCtrl.login = function () {
-      Auth.$authWithPassword(authCtrl.user).then(function (auth) {
-          $state.go('home');
-      }, function (error) {
-          authCtrl.error = error;
-      });
-  };
-
-  // Registers new user
-  authCtrl.register = function () {
-      Auth.$createUser(authCtrl.user).then(function (user) {
-          authCtrl.login();
-      }, function (error) {
-          authCtrl.error = error;
-      });
-  };
