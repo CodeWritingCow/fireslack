b498fe8782add9aaf567e54c5c79d19c455ef97c
Gary Pang <gary.pang@gmail.com>
2015-12-01 01:18:15 -0500

    Create 'channels.direct' state
    Users can send direct messages to each other.

--- app/app.js
+++ app/app.js
@@ -114,6 +114,21 @@ angular
                   return '#'+channels.$getRecord($stateParams.channelId).name;
               }
           }
+      })
+      .state('channels.direct', {
+          url: '/{uid}/messages/direct',
+          templateUrl: 'channels/messages.html',
+          controller: 'MessagesCtrl as messagesCtrl',
+          resolve: {
+              messages: function($stateParams, Messages, profile){
+                  return Messages.forUsers($stateParams.uid, profile.$id).$loaded();
+              },
+              channelName: function($stateParams, Users){
+                  return Users.all.$loaded().then(function(){
+                      return '@'+Users.getDisplayName($stateParams.uid);
+                  });
+              }
+          }
       });
 
       $urlRouterProvider.otherwise('/');
--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -4,6 +4,7 @@ angular.module('angularfireSlackApp')
 
       channelsCtrl.profile = profile;
       channelsCtrl.channels = channels;
+      channelsCtrl.users = Users.all;
 
       channelsCtrl.getDisplayName = Users.getDisplayName;
       channelsCtrl.getGravatar = Users.getGravatar;
@@ -23,5 +24,5 @@ angular.module('angularfireSlackApp')
               $state.go('channels.messages', { channelId: ref.key() });
           });
       };
-      
+
   });
